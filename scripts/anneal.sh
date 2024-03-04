9ef8c52adae51ff38d7fd518373e28355b51c1ac
Mitch Gildenberg <mitch@switchboard.xyz>
2024-03-04 15:17:01 -0500

    fixups

--- scripts/anneal.sh
+++ scripts/anneal.sh
@@ -2,6 +2,10 @@ if [ "$1" == "devnet" ]; then
     # namespace == devnet-pull
     az aks get-credentials --resource-group Default --name multichain-devnet
     helm upgrade -i pull-oracle-devnet ./charts/pull-service/ -f ./chains/solana/devnet-pull.yaml
+elif [ "$1" == "equinix-devnet" ]; then
+    # namespace == devnet-pull
+    cp ~/.kube/config-equinix ~/.kube/config
+    helm upgrade -i pull-oracle-devnet ./charts/pull-service/ -f ./chains/solana/devnet-pull-equinix.yaml
 else
     echo "Invalid argument. Please use either 'mainnet' or 'devnet'."
 fi
