a67029b8c5a88e8d14963335dd24a16d122824e0
Mitch Gildenberg <mitch@switchboard.xyz>
2024-03-04 09:09:37 -0500

    fixups

--- scripts/anneal.sh
+++ scripts/anneal.sh
@@ -1,4 +1,5 @@
 if [ "$1" == "devnet" ]; then
+    # namespace == devnet-pull
     az aks get-credentials --resource-group Default --name multichain-devnet
     helm upgrade -i pull-oracle-devnet ./charts/pull-service/ -f ./chains/solana/devnet-pull.yaml
 else
