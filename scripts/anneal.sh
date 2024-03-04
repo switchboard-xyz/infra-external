a5f9c4505da41bed1b2c98f2b4c52a262aa8a824
Mitch Gildenberg <mitch@switchboard.xyz>
2024-03-04 08:23:01 -0500

    fixups

---
+++ scripts/anneal.sh
@@ -0,0 +1,7 @@
+if [ "$1" == "devnet" ]; then
+    az aks get-credentials --resource-group Default --name multichain-devnet
+    helm upgrade -i pull-oracle-devnet ./charts/pull-service/ -f ./chains/solana/devnet-pull.yaml
+else
+    echo "Invalid argument. Please use either 'mainnet' or 'devnet'."
+fi
+
