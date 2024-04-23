815cf964d2f7a8014ff5a8de9235d5ac5ec44c73
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2024-04-24 00:18:14 +0200

    script: fix sed and add helm create-namespace

--- scripts/install/99-k8s-oracle-install.sh
+++ scripts/install/99-k8s-oracle-install.sh
@@ -7,15 +7,16 @@ source ./00-vars.cfg
 helm_values_files="../../chains/solana/devnet-pull.yaml"
 helm_chart_dir="../../charts/pull-service/"
 
-sed -i "s/__INFISICAL_SECRETS_PATH__/${INFISICAL_SECRETS_PATH}/g" "${helm_values_files}"
-sed -i "s/__INFISICAL_SLUG__/${INFISICAL_SLUG}/g" "${helm_values_files}"
-sed -i "s/__CLUSTER_DOMAIN__/${CLUSTER_DOMAIN}/g" "${helm_values_files}"
-sed -i "s/__GUARDIAN_QUEUE__/${GUARDIAN_QUEUE}/g" "${helm_values_files}"
-sed -i "s/__GUARDIAN_ORACLE1__/${GUARDIAN_ORACLE1}/g" "${helm_values_files}"
-sed -i "s/__ORACLE1__/${ORACLE1}/g" "${helm_values_files}"
-sed -i "s/__QUEUE__/${QUEUE}/g" "${helm_values_files}"
+sed -i 's?__INFISICAL_SECRETS_PATH__?'"${INFISICAL_SECRETS_PATH}"'?g' "${helm_values_files}"
+sed -i 's?__INFISICAL_SLUG__?'"${INFISICAL_SLUG}"'?g' "${helm_values_files}"
+sed -i 's?__CLUSTER_DOMAIN__?'"${CLUSTER_DOMAIN}"'?g' "${helm_values_files}"
+sed -i 's?__GUARDIAN_QUEUE__?'"${GUARDIAN_QUEUE}"'?g' "${helm_values_files}"
+sed -i 's?__GUARDIAN_ORACLE1__?'"${GUARDIAN_ORACLE1}"'?g' "${helm_values_files}"
+sed -i 's?__ORACLE1__?'"${ORACLE1}"'?g' "${helm_values_files}"
+sed -i 's?__QUEUE__?'"${QUEUE}"'?g' "${helm_values_files}"
 
 # install the helm chart
 helm upgrade -i pull-oracle-devnet \
+	-n "${NAMESPACE}" --create-namespace \
 	-f "${helm_values_files}" \
 	"${helm_chart_dir}"
