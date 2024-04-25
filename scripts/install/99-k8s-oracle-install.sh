0ddfa0b9d4e3d415eb4d96915acafe5bb1fc79d2
Mitch Gildenberg <mitch@switchboard.xyz>
2024-04-25 15:34:53 -0400

    cp

--- scripts/install/99-k8s-oracle-install.sh
+++ scripts/install/99-k8s-oracle-install.sh
@@ -9,13 +9,13 @@ helm_chart_dir="../../charts/pull-service/"
 tmp_helm_file="/tmp/helm_values.yaml"
 cp "${helm_values_files}" "${tmp_helm_file}"
 
-sed 's?__INFISICAL_SECRETS_PATH__?'"${INFISICAL_SECRETS_PATH}"'?g' "${tmp_helm_file}"
-sed 's?__INFISICAL_SLUG__?'"${INFISICAL_SLUG}"'?g' "${tmp_helm_file}"
-sed 's?__CLUSTER_DOMAIN__?'"${CLUSTER_DOMAIN}"'?g' "${tmp_helm_file}"
-sed 's?__GUARDIAN_QUEUE__?'"${GUARDIAN_QUEUE}"'?g' "${tmp_helm_file}"
-sed 's?__GUARDIAN_ORACLE1__?'"${GUARDIAN_ORACLE1}"'?g' "${tmp_helm_file}"
-sed 's?__ORACLE1__?'"${ORACLE1}"'?g' "${tmp_helm_file}"
-sed 's?__QUEUE__?'"${QUEUE}"'?g' "${tmp_helm_file}"
+sed 's?__INFISICAL_SECRETS_PATH__?'"${INFISICAL_SECRETS_PATH}"'?g' > "${tmp_helm_file}"
+sed 's?__INFISICAL_SLUG__?'"${INFISICAL_SLUG}"'?g' > "${tmp_helm_file}"
+sed 's?__CLUSTER_DOMAIN__?'"${CLUSTER_DOMAIN}"'?g' > "${tmp_helm_file}"
+sed 's?__GUARDIAN_QUEUE__?'"${GUARDIAN_QUEUE}"'?g' > "${tmp_helm_file}"
+sed 's?__GUARDIAN_ORACLE1__?'"${GUARDIAN_ORACLE1}"'?g' > "${tmp_helm_file}"
+sed 's?__ORACLE1__?'"${ORACLE1}"'?g' > "${tmp_helm_file}"
+sed 's?__QUEUE__?'"${QUEUE}"'?g' > "${tmp_helm_file}"
 
 # install the helm chart
 helm upgrade -i pull-oracle-${NETWORK} \
