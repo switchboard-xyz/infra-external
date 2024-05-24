a87bb999ccda8a9447f36d1be731a8515e65009f
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2024-05-24 19:41:18 +0200

    SB: fix Account creation script

--- .scripts/solana/40-oracle-ctr-sol.sh
+++ .scripts/solana/40-oracle-ctr-sol.sh
@@ -2,10 +2,14 @@
 set -u -e
 
 # import vars
-source ./00-vars.cfg
+source ../../../cfg/00-vars.cfg
 
-export host_dir="$(pwd)/../src"
-export ctr_dir="/app"
+export data_host_dir="../../../data"
+export data_ctr_dir="/data"
+
+export script_host_dir="$(pwd)"
+export script_ctr_dir="/app"
+export script_filename="41-oracle-create-sol-account.sh"
 
 CTR_NAME="CTR-sg-tmp-solana"
 set +e
@@ -18,15 +22,17 @@ export image="docker.io/solanalabs/solana:stable"
 if [[ "${use_docker}" != "--docker" ]]; then
 	ctr i pull "${image}"
 	ctr run -t --net-host \
-		--rm --cwd "${ctr_dir}" \
-		--mount type=bind,src=${host_dir},dst="${ctr_dir}",options=rbind:rw \
+		--rm --cwd "${script_ctr_dir}" \
+		--mount "type=bind,src=${data_host_dir},dst=${data_ctr_dir},options=rbind:rw" \
+		--mount "type=bind,src=${script_host_dir}/${script_filename},dst=${script_ctr_dir}/${script_filename},options=rbind:rw" \
 		"${image}" "${CTR_NAME}" /bin/bash
 else
 
 	docker image pull "${image}"
 	docker run -ti --rm \
-		--workdir "${ctr_dir}" \
-		--mount type=bind,src=${host_dir},dst="${ctr_dir}" \
+		--workdir "${script_ctr_dir}" \
+		-v "${data_host_dir}/:${data_ctr_dir}/" \
+		-v "${script_host_dir}/${script_filename}:${script_ctr_dir}/${script_filename}" \
 		--entrypoint /bin/bash \
 		--name "${CTR_NAME}" \
 		"${image}"
