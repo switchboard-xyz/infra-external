b63004c5caaa577fe9f35f28039a7d0937b6c973
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2024-05-29 01:21:32 +0200

    apt: fix option

--- .scripts/var/00-docker-install.sh
+++ .scripts/var/00-docker-install.sh
@@ -3,5 +3,5 @@ set -u -e
 
 sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt install docker-ce
