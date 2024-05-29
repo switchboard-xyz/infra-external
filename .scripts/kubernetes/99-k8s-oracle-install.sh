f1f8c8e1fabc5d032777f48e5a599edd3be974ea
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2024-05-29 03:16:25 +0200

    git: isolate load_vars into its own file

--- .scripts/kubernetes/99-k8s-oracle-install.sh
+++ .scripts/kubernetes/99-k8s-oracle-install.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 set -u -e
 
-# import vars
-source ../../../cfg/00-vars.cfg
-
 helm_values_files="../../chains/solana/values.yaml"
 helm_chart_dir="../../charts/pull-service/"
 tmp_helm_file="/tmp/helm_values.yaml"
 cp "${helm_values_files}" "${tmp_helm_file}"
 
+source ../../../cfg/00-vars.cfg
+source ../../.scripts/var/_load_vars.sh
 load_vars "${tmp_helm_file}"
 
 # install the helm chart
