ffe75c8204e9d81b09084caee3ec8b5c942ebb32
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2024-07-01 16:11:06 +0200

    sgx: check script fix

--- .scripts/sgx/13-sgx-check-sa.sh
+++ .scripts/sgx/13-sgx-check-sa.sh
@@ -2,20 +2,27 @@ export use_docker="${1:-''}"
 
 export SGX_CHECK_IMAGE="docker.io/switchboardlabs/pull-oracle:stable"
 
+export DATA_DIR="../../../data/devnet_protected_files"
+
+mkdir -p $DATA_DIR
+
 if [[ "${use_docker}" != "--docker" ]]; then
 	ctr i pull "${SGX_CHECK_IMAGE}"
 	ctr run \
 		--privileged -t --rm --net-host \
-		--env ENABLE_GATEWAY=1 --env LIST_CONFIG_AND_EXIT=true \
+		--env ENABLE_GATEWAY=1 \
+		--env LIST_CONFIG_AND_EXIT=true \
 		--mount type=bind,options=rbind:rw,src=/var/run/aesmd,dst=/var/run/aesmd \
 		--mount src=/dev/sgx,dst=/dev/sgx,type=bind,options=rbind:rw \
+		--mount src="${DATA_DIR}/",dst=/data/protected_files/,options=rbind:rw \
 		"${SGX_CHECK_IMAGE}" test_sgx
 else
 	docker run \
 		--privileged -it --rm --network host \
-		-v /var/run/aesmd:/var/run/aesmd \
-		-v /dev/sgx:/dev/sgx \
 		-e ENABLE_GATEWAY=1 \
 		-e LIST_CONFIG_AND_EXIT=true \
+		-v /var/run/aesmd:/var/run/aesmd \
+		-v /dev/sgx:/dev/sgx \
+		-v "${DATA_DIR}/":/data/protected_files/ \
 		"${SGX_CHECK_IMAGE}"
 fi
