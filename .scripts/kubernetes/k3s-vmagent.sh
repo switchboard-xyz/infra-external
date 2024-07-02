4bff65d86ff78172d7d3c3eb7f91d36595c13a41
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2024-07-03 01:04:32 +0200

    k8s oracle: fix vmagent script

--- .scripts/kubernetes/k3s-vmagent.sh
+++ .scripts/kubernetes/k3s-vmagent.sh
@@ -7,14 +7,14 @@ source ../../../cfg/00-common-vars.cfg
 cluster="${1:-devnet}"
 
 if [[ "${cluster}" == "mainnet-beta" ]]; then
-	cluster="mainnet"
+  cluster="mainnet"
 fi
 
 if [[ "${cluster}" != "devnet" &&
-	"${cluster}" != "mainnet" &&
-	"${cluster}" != "mainnet-beta" ]]; then
-	echo "Only valid cluster values are 'devnet' and 'mainnet'/'mainnet-beta'."
-	exit 1
+  "${cluster}" != "mainnet" &&
+  "${cluster}" != "mainnet-beta" ]]; then
+  echo "Only valid cluster values are 'devnet' and 'mainnet'/'mainnet-beta'."
+  exit 1
 fi
 
 source "../../../cfg/00-${cluster}-vars.cfg"
@@ -24,16 +24,18 @@ helm repo update
 
 VMAGENT_NS="vmagent-${cluster}"
 if [[ "$(kubectl get ns | grep -e '^'${VMAGENT_NS}'\W')" == "" ]]; then
-	kubectl create namespace "${VMAGENT_NS}"
+  kubectl create namespace "${VMAGENT_NS}"
 fi
 
+kubectl delete configmap -n "${VMAGENT_NS}" vmagent-env >/dev/null 2>&1
+
 kubectl create configmap \
-	-n "${VMAGENT_NS}" \
-	--from-file="../../../cfg/00-common-vars.cfg" \
-	--from-file="../../../cfg/00-${cluster}-vars.cfg" \
-	vmagent-env
+  -n "${VMAGENT_NS}" \
+  --from-file="../../../cfg/00-common-vars.cfg" \
+  --from-file="../../../cfg/00-${cluster}-vars.cfg" \
+  vmagent-env
 
 helm upgrade -i "vmagent-${cluster}" \
-	-n "${VMAGENT_NS}" \
-	-f "../../../.scripts/kubernetes/vmagent.yaml" \
-	vm/victoria-metrics-agent
+  -n "${VMAGENT_NS}" \
+  -f "../../../.scripts/kubernetes/vmagent.yaml" \
+  vm/victoria-metrics-agent
