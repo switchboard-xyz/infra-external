a800cdfa8458b5c1e27af9e9bd0b8d3de660a939
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-02-03 03:21:42 +0100

    sail-setup: fix script

--- .scripts/sail/sail-setup.sh
+++ .scripts/sail/sail-setup.sh
@@ -1,15 +1,14 @@
-# kernel
 TMPDIR="$(mktemp -d)"
-wget -q 'https://sapphire-perfect-gerbil-428.mypinata.cloud/ipfs/bafybeiefyfwhxurut6n4wvz53zad5ffyt7vrllq5eomv6nejvqqlosbfpi' -O "${TMPDIR}/sb-sail-kernel.tar.xz"
 
-#initrd
+cd "${TMPDIR}"
+wget -q 'https://sapphire-perfect-gerbil-428.mypinata.cloud/ipfs/bafybeiefyfwhxurut6n4wvz53zad5ffyt7vrllq5eomv6nejvqqlosbfpi' -O "${TMPDIR}/sb-sail-kernel.tar.xz"
 wget -q 'https://sapphire-perfect-gerbil-428.mypinata.cloud/ipfs/bafybeibqtndt7xeaasrbg5fkvdv24jysjkzxna3rgjum5fxpxamojhxclu' -O "${TMPDIR}/sb-sail-initrd.tar.xz"
 
-tar --transform 's/.*/sb-sail-initrd/' -xf "${TMPDIR}/sb-sail-initrd.tar.xz" ./opt/kata/share/kata-containers/kata-ubuntu-20.04-confidential.initrd -C /opt/kata/share/kata-containers/sb-sail-initrd
+tar --transform 's/.*/sb-sail-initrd/' -xf "${TMPDIR}/sb-sail-initrd.tar.xz" ./opt/kata/share/kata-containers/kata-ubuntu-20.04-confidential.initrd
+tar --transform 's/.*/sb-sail-kernel/' -xf "${TMPDIR}/sb-sail-kernel.tar.xz" ./opt/kata/share/kata-containers/vmlinuz-6.12.8-142-confidential
 
-tar --transform 's/.*/sb-sail-kernel/' -xf "${TMPDIR}/sb-sail-kernel.tar.xz" ./opt/kata/share/kata-containers/vmlinuz-6.12.8-142-confidential -C /opt/kata/share/kata-containers/
+mv ./sb-sail* /opt/kata/share/kata-containers/
 
-sed -i '' /opt/kata/share/defaults/kata-containers/configuration-qemu-snp.toml
 sed -i 's?^initrd =.*?initrd = /opt/kata/share/kata-containers/sb-sail-initrd?g' /opt/kata/share/defaults/kata-containers/configuration-qemu-snp.toml
 sed -i 's?^kernel =.*?kernel = /opt/kata/share/kata-containers/sb-sail-kernel?g' /opt/kata/share/defaults/kata-containers/configuration-qemu-snp.toml
 
