67d25d312619546e58953e62c5b49b157d38d22a
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-02-03 03:02:24 +0100

    sail-setup: fix script

--- .scripts/sail/sail-setup.sh
+++ .scripts/sail/sail-setup.sh
@@ -5,8 +5,8 @@ wget -q 'https://sapphire-perfect-gerbil-428.mypinata.cloud/ipfs/bafybeiefyfwhxu
 #initrd
 wget -q 'https://sapphire-perfect-gerbil-428.mypinata.cloud/ipfs/bafybeibqtndt7xeaasrbg5fkvdv24jysjkzxna3rgjum5fxpxamojhxclu' -O "${TMPDIR}/sb-sail-initrd.tar.xz"
 
-tar -x "${TMPDIR}/sb-sail-kernel.tar.xz" -C /opt/kata/share/kata-containers/
-tar -x "${TMPDIR}/sb-sail-initrd.tar.xz" -C /opt/kata/share/kata-containers/
+tar -xf "${TMPDIR}/sb-sail-kernel.tar.xz" -C /opt/kata/share/kata-containers/
+tar -xf "${TMPDIR}/sb-sail-initrd.tar.xz" -C /opt/kata/share/kata-containers/
 
 sed -i '' /opt/kata/share/defaults/kata-containers/configuration-qemu-snp.toml
 sed -i 's?^initrd =.*?initrd = /opt/kata/share/kata-containers/sb-sail-initrd?g' /opt/kata/share/defaults/kata-containers/configuration-qemu-snp.toml
