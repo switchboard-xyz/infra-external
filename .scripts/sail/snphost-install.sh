685a711bbd6498bad4cd28a5e60106e714a7ee99
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-02-06 02:15:41 +0100

    sail: fix snphost install script #2

--- .scripts/sail/snphost-install.sh
+++ .scripts/sail/snphost-install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -eu
 
-wget -q 'https://sapphire-perfect-gerbil-428.mypinata.cloud/ipfs/bafybeiatox2alhmpm3hy4mvvonzzdkkq3mvp3zskvjztnsat2jbnryuikq' -O "/usr/local/bin/snphost"
+snphost_binary="/usr/local/bin/snphost"
+
+wget -q 'https://sapphire-perfect-gerbil-428.mypinata.cloud/ipfs/bafybeiatox2alhmpm3hy4mvvonzzdkkq3mvp3zskvjztnsat2jbnryuikq' -O "${snphost_binary}"
+
+chmod 755 "${snphost_binary}"
 
 echo "SNPHOST installed in /usr/local/bin/snphost"
