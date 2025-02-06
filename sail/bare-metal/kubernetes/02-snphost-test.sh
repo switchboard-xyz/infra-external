26e8dbafa31792ac90d603cb05fe798bb1da5bff
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-02-06 02:11:07 +0100

    sail: add snphost installation

---
+++ sail/bare-metal/kubernetes/02-snphost-test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -eu
+
+wget -q 'https://sapphire-perfect-gerbil-428.mypinata.cloud/ipfs/bafybeiatox2alhmpm3hy4mvvonzzdkkq3mvp3zskvjztnsat2jbnryuikq' -O "/usr/local/bin/snphost"
+
+echo "SNPHOST installed in /usr/local/bin/snphost"
