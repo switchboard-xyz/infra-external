d828f3c7b008385265c259cc2d0f2b823ed96129
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-02-07 21:45:06 +0100

    ipfs: change URL

--- .scripts/sail/sail-setup.sh
+++ .scripts/sail/sail-setup.sh
@@ -1,8 +1,8 @@
 TMPDIR="$(mktemp -d)"
 
 cd "${TMPDIR}"
-wget -q 'https://sapphire-perfect-gerbil-428.mypinata.cloud/ipfs/bafybeiebhqp5ckhzqdumua5swmqihacsap7w7kz5cu7kbgd35bxsckp7sq' -O "${TMPDIR}/sb-sail-initrd"
-wget -q 'https://sapphire-perfect-gerbil-428.mypinata.cloud/ipfs/bafybeidpqahfmvugxxizufa6aabefrv53e7cr5dtfccmkbme7jsmlz3zvq' -O "${TMPDIR}/sb-sail-kernel"
+wget -q 'https://ipfs.io/ipfs/bafybeiebhqp5ckhzqdumua5swmqihacsap7w7kz5cu7kbgd35bxsckp7sq' -O "${TMPDIR}/sb-sail-initrd"
+wget -q 'https://ipfs.io/ipfs/bafybeidpqahfmvugxxizufa6aabefrv53e7cr5dtfccmkbme7jsmlz3zvq' -O "${TMPDIR}/sb-sail-kernel"
 
 sha256sum "${TMPDIR}/sb-sail-kernel"
 sha256sum "${TMPDIR}/sb-sail-initrd"
