2797164602443e16a78d2df0049d91cadce375a7
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-02-07 19:04:45 +0100

    sail: change pinata files

--- .scripts/sail/sail-setup.sh
+++ .scripts/sail/sail-setup.sh
@@ -1,8 +1,11 @@
 TMPDIR="$(mktemp -d)"
 
 cd "${TMPDIR}"
-wget -q 'https://sapphire-perfect-gerbil-428.mypinata.cloud/ipfs/bafybeidpqahfmvugxxizufa6aabefrv53e7cr5dtfccmkbme7jsmlz3zvq' -O "${TMPDIR}/sb-sail-kernel"
 wget -q 'https://sapphire-perfect-gerbil-428.mypinata.cloud/ipfs/bafybeiebhqp5ckhzqdumua5swmqihacsap7w7kz5cu7kbgd35bxsckp7sq' -O "${TMPDIR}/sb-sail-initrd"
+wget -q 'https://sapphire-perfect-gerbil-428.mypinata.cloud/ipfs/bafybeidpqahfmvugxxizufa6aabefrv53e7cr5dtfccmkbme7jsmlz3zvq' -O "${TMPDIR}/sb-sail-kernel"
+
+sha256sum "${TMPDIR}/sb-sail-kernel"
+sha256sum "${TMPDIR}/sb-sail-initrd"
 
 mv ./sb-sail* /opt/kata/share/kata-containers/
 
