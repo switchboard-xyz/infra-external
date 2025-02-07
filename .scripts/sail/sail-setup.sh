9ed01d013b977d8c2a2bea8477c4877f4aff3ad7
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-02-07 18:58:13 +0100

    sail: change pinata files

--- .scripts/sail/sail-setup.sh
+++ .scripts/sail/sail-setup.sh
@@ -1,11 +1,8 @@
 TMPDIR="$(mktemp -d)"
 
 cd "${TMPDIR}"
-wget -q 'https://sapphire-perfect-gerbil-428.mypinata.cloud/ipfs/bafybeiakvn5kwqkd4litjzbflz7hqmmyzz66fd2wnpildtmx5jrnwwyhme' -O "${TMPDIR}/sb-sail-kernel.tar.xz"
-wget -q 'https://sapphire-perfect-gerbil-428.mypinata.cloud/ipfs/bafybeicj6mjvukjpdkonhdd7xkx5xxpbeu56si7246avoighahhg6wf4ae' -O "${TMPDIR}/sb-sail-initrd.tar.xz"
-
-tar --transform 's/.*/sb-sail-initrd/' -xf "${TMPDIR}/sb-sail-initrd.tar.xz" ./opt/kata/share/kata-containers/kata-ubuntu-20.04-confidential.initrd
-tar --transform 's/.*/sb-sail-kernel/' -xf "${TMPDIR}/sb-sail-kernel.tar.xz" ./opt/kata/share/kata-containers/vmlinuz-6.12.8-142-confidential
+wget -q 'https://sapphire-perfect-gerbil-428.mypinata.cloud/ipfs/bafybeidpqahfmvugxxizufa6aabefrv53e7cr5dtfccmkbme7jsmlz3zvq' -O "${TMPDIR}/sb-sail-kernel"
+wget -q 'https://sapphire-perfect-gerbil-428.mypinata.cloud/ipfs/bafybeiebhqp5ckhzqdumua5swmqihacsap7w7kz5cu7kbgd35bxsckp7sq' -O "${TMPDIR}/sb-sail-initrd"
 
 mv ./sb-sail* /opt/kata/share/kata-containers/
 
