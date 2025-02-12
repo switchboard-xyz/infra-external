d7b3232789371b62aea16031ed2f995d0b70bf05
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-02-12 20:10:52 +0100

    scripts: fix scripts #2

--- oracle/bare-metal/kubernetes/91-k8s-ctr-cleanup.sh
+++ oracle/bare-metal/kubernetes/91-k8s-ctr-cleanup.sh
@@ -1,13 +1,1 @@
-#!/usr/bin/env bash
-set -u -e
-
-for i in \
-  $(k3s \
-    ctr \
-    --address /run/k3s/containerd/containerd.sock \
-    --namespace k8s.io i ls -q |
-    awk '{print $1}'); do
-  k3s ctr \
-    --address /run/k3s/containerd/containerd.sock \
-    --namespace k8s.io i rm $i
-done
+../../../.scripts/sail/ctr-cleanup.sh
