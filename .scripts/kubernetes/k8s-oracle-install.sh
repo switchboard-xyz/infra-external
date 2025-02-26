2dd7ea0860b57cfe2575bebc004e794e24f3697e
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-02-26 15:36:09 +0100

    helm: more cleanup

--- .scripts/kubernetes/k8s-oracle-install.sh
+++ .scripts/kubernetes/k8s-oracle-install.sh
@@ -65,12 +65,13 @@ helm upgrade -i "sb-oracle-${NETWORK}" \
   -n "${NAMESPACE}" --create-namespace \
   -f "${helm_default_values_file}" \
   -f "${tmp_helm_file}" \
-  --set components.oracle.enabled="${ORACLE_ENABLED}" \
   --set components.docker_image_tag="${DOCKER_IMAGE_TAG}" \
+  --set components.oracle.enabled=${ORACLE_ENABLED} \
   --set components.oracle.image="${ORACLE_DOCKER_IMAGE}" \
+  --set components.guardian.enabled=${GUARDIAN_ENABLED} \
   --set components.guardian.image="${GUARDIAN_DOCKER_IMAGE}" \
+  --set components.gateway.enabled=${GUARDIAN_ENABLED} \
   --set components.gateway.image="${GATEWAY_DOCKER_IMAGE}" \
-  --set components.guardian.enabled="${GUARDIAN_ENABLED}" \
   "${helm_chart_dir}" >/dev/null
 echo "HELM: Switchboard Oracle installed under namespace ${NAMESPACE}"
 
