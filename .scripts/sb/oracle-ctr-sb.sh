c328fd9e7185e3359b49b0eb1c0945264a8e834c
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-03-22 04:41:59 +0100

    scripts: add new script, move around others

--- .scripts/sb/oracle-ctr-sb.sh
+++ .scripts/sb/oracle-ctr-sb.sh
@@ -7,7 +7,7 @@ export data_ctr_dir="/data"
 export script_host_dir="$(pwd)"
 export script_ctr_dir="/app"
 export prep_script_filename="51-oracle-prepare-request.sh"
-export check_script_filename="52-oracle-check-perms.sh"
+export ncn_enroll_script_filename="52-oracle-ncn-enroll.sh"
 
 export image="docker.io/switchboardlabs/sb-utils:3.5.10"
 
@@ -25,5 +25,5 @@ k3s ctr run -t --net-host \
   --rm --cwd "${script_ctr_dir}" \
   --mount "type=bind,src=${data_host_dir},dst=${data_ctr_dir},options=rbind:rw" \
   --mount "type=bind,src=${script_host_dir}/${prep_script_filename},dst=${script_ctr_dir}/${prep_script_filename},options=rbind:rw" \
-  --mount "type=bind,src=${script_host_dir}/${check_script_filename},dst=${script_ctr_dir}/${check_script_filename},options=rbind:rw" \
+  --mount "type=bind,src=${script_host_dir}/${ncn_enroll_script_filename},dst=${script_ctr_dir}/${ncn_enroll_script_filename},options=rbind:rw" \
   "${image}" "${CTR_NAME}" /bin/bash
