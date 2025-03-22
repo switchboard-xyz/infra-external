ddb046028f72c74b63f64d85c41380b9a416d3b2
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-03-22 05:01:15 +0100

    scripts: start using printf in scripts #2

--- .scripts/sb/oracle-prepare-request.sh
+++ .scripts/sb/oracle-prepare-request.sh
@@ -5,7 +5,7 @@ priorityFee="${2:-10000}"
 
 if [[ "${cluster}" != "devnet" &&
   "${cluster}" != "mainnet" ]]; then
-  printf "Only valid cluster values are 'devnet' and 'mainnet'"
+  printf "Only valid cluster values are 'devnet' and 'mainnet'\n"
   exit 1
 fi
 
@@ -19,65 +19,65 @@ elif [[ "${cluster}" == "mainnet" ]]; then
   queueKey="A43DyUGA7s8eXPxqEjJY6EBu1KKbNgfxF8h17VAHn13w"
 fi
 
-printf " "
-printf "=========================================================================="
-printf "||                                                                      ||"
-printf "||                 YOU ARE NOW IN A TEMPORARY CONTAINER                 ||"
-printf "||          PLEASE FOLLOW THE INSTRUCTIONS BELOW AND THE DOCS           ||"
-printf "||                                                                      ||"
-printf "=========================================================================="
-printf " "
+printf "\n"
+printf "==========================================================================\n"
+printf "||                                                                      ||\n"
+printf "||                 YOU ARE NOW IN A TEMPORARY CONTAINER                 ||\n"
+printf "||          PLEASE FOLLOW THE INSTRUCTIONS BELOW AND THE DOCS           ||\n"
+printf "||                                                                      ||\n"
+printf "==========================================================================\n"
+printf "\n"
 
 # change `mainnet` to `mainnet-beta` for historical reasons
 if [[ "${cluster}" == "mainnet" ]]; then
   cluster="mainnet-beta"
 fi
 
-printf " "
+printf "\n"
 export register_oracle=""
 while [[ 
   "${register_oracle}" != "y" &&
   "${register_oracle}" != "Y" &&
   "${register_oracle}" != "n" &&
   "${register_oracle}" != "N" ]]; do
-  printf -n "Do you want to register an oracle? (y/n) "
+  printf "Do you want to register an oracle? (y/n) "
   read -r register_oracle
 done
 
-printf " "
+printf "\n"
 export register_guardian=""
 while [[ 
   "${register_guardian}" != "y" &&
   "${register_guardian}" != "Y" &&
   "${register_guardian}" != "n" &&
   "${register_guardian}" != "N" ]]; do
-  printf -n "Do you want to register a guardian? (y/n) "
+  printf "Do you want to register a guardian? (y/n) "
   read -r register_guardian
 done
 
-printf " "
-printf "=========================================================================="
-printf "||                       !!! IMPORTANT NOTICE !!!                       ||"
-printf "=========================================================================="
-printf "||                                                                      ||"
-printf "||                 >>> COPY/SAVE THE OUTPUT FROM HERE <<<               ||"
-printf "||                                                                      ||"
-printf "||  -> Solana cluster: %-8s %40s" "${cluster}" "||"
-printf "||  -> queueKey: %44s %30s" "${queueKey}" "||"
-printf "||                                                                      ||"
-printf "||  Creating new registration request for:                              ||"
+printf "\n"
+printf "==========================================================================\n"
+printf "||                       !!! IMPORTANT NOTICE !!!                       ||\n"
+printf "==========================================================================\n"
+printf "||                                                                      ||\n"
+printf "||                 >>> COPY/SAVE THE OUTPUT FROM HERE <<<               ||\n"
+printf "||                                                                      ||\n"
+printf "||  -> Solana cluster: %-8s %40s\n" "${cluster}" "||"
+printf "||  -> queueKey: %44s %30s\n" "${queueKey}" "||"
+printf "||                                                                      ||\n"
+printf "||  Creating new registration request for:                              ||\n"
 if [[ "${register_oracle}" == "y" || "${register_oracle}" == "Y" ]]; then
-  printf "||    -> ORACLE                                                         ||"
+  printf "||    -> ORACLE                                                         ||\n"
 fi
 if [[ "${register_guardian}" == "y" || "${register_guardian}" == "Y" ]]; then
-  printf "||    -> GUARDIAN                                                       ||"
+  printf "||    -> GUARDIAN                                                       ||\n"
 fi
-printf "||                                                                      ||"
-printf "=========================================================================="
-printf " "
+printf "||                                                                      ||\n"
+printf "==========================================================================\n"
+printf "\n"
 
 if [[ "${register_oracle}" == "y" || "${register_oracle}" == "Y" ]]; then
-  printf " "
+  printf "\n"
   if [[ "${DEBUG}" == "true" ]]; then
     (
       sb solana on-demand oracle create \
@@ -98,7 +98,7 @@ if [[ "${register_oracle}" == "y" || "${register_oracle}" == "Y" ]]; then
 fi
 
 if [[ "${register_guardian}" == "y" || "${register_guardian}" == "Y" ]]; then
-  printf " "
+  printf "\n"
   if [[ "${DEBUG}" == "true" ]]; then
     (
       sb solana on-demand guardian create \
@@ -116,53 +116,53 @@ if [[ "${register_guardian}" == "y" || "${register_guardian}" == "Y" ]]; then
   fi
 fi
 
-printf " "
-printf "=========================================================================="
-printf "||                       !!! IMPORTANT NOTICE !!!                       ||"
-printf "=========================================================================="
-printf "||                                                                      ||"
-printf "|| The output above contains your Oracle/Guardian public keys and       ||"
-printf "|| related data.                                                        ||"
-printf "||                                                                      ||"
-printf "|| It's safe to share your PUBKEYs for your Oracle and Guardian as the  ||"
-printf "|| the name implies they are in fact public.                            ||"
-printf "||                                                                      ||"
-printf "|| PUBKEYs example:                                                     ||"
-printf "||   PULL_ORACLE=AbCdEfGhIjKlMnOpQrStUvXwYz1234567890AbCdEfGh           ||"
-printf "||   GUARDIAN_ORACLE=AbCdEfGhIjKlMnOpQrStUvXwYz1234567890AbCdEfGh       ||"
-printf "||                                                                      ||"
-printf "|| The QUEUE variables are fixed among all Oracles & Guardians as they  ||"
-printf "|| represent the Solana queues we use to coordinate operations on chain ||"
-printf "|| and only change between devnet vs mainnet.                           ||"
-printf "||                                                                      ||"
-printf "|| QUEUE devnet vars :                                                  ||"
-printf "||   PULL_QUEUE=EYiAmGSdsQTuCw413V5BzaruWuCCSDgTPtBGvLkXHbe7            ||"
-printf "||   GUARDIAN_QUEUE=BeZ4tU4HNe2fGQGUzJmNS2UU2TcZdMUUgnCH6RPg4Dpi        ||"
-printf "|| QUEUE mainnet vars :                                                 ||"
-printf "||   PULL_QUEUE=A43DyUGA7s8eXPxqEjJY6EBu1KKbNgfxF8h17VAHn13w            ||"
-printf "||   GUARDIAN_QUEUE=B7WgdyAgzK7yGoxfsBaNnY6d41bTybTzEh4ZuQosnvLK        ||"
-printf "||                                                                      ||"
-printf "|| SECURITY WARNING:                                                    ||"
-printf "||  Keep your 24-word seed phrase and private key (array-like number)   ||"
-printf "||                         ABSOLUTELY PRIVATE!                          ||"
-printf "||                                                                      ||"
-printf "|| Next steps:                                                          ||"
-printf "||   1. Edit cfg/00-devnet-vars.cfg or cfg/00-mainnet-vars.cfg file     ||"
-printf "||      and insert the output in the proper variables                   ||"
-printf "||                                                                      ||"
-printf "||   2. Submit a request for approval of your Oracle/Guardian data at:  ||"
-printf "||                                                                      ||"
-printf "||      https://forms.gle/2xWwFQ8XPBGu9DRL6                             ||"
-printf "||                                                                      ||"
-printf "||      In the form you should just submit the values of                ||"
-printf "||        PULL_ORACLE     + PULL_QUEUE                                  ||"
-printf "||                                                                      ||"
-printf "||      and only if you are runnning a guardian also send               ||"
-printf "||        GUARDIAN_ORACLE + GUARDIAN_QUEUE                              ||"
-printf "||                                                                      ||"
-printf "||   3. Please save the output above and then you can exit now          ||"
-printf "||      Wait for approval before proceeding with the following steps    ||"
-printf "||                                                                      ||"
-printf "||          SAVE THIS OUTPUT NOW, THEN TYPE 'exit' TO CONTINUE          ||"
-printf "=========================================================================="
-printf " "
+printf "\n"
+printf "==========================================================================\n"
+printf "||                       !!! IMPORTANT NOTICE !!!                       ||\n"
+printf "==========================================================================\n"
+printf "||                                                                      ||\n"
+printf "|| The output above contains your Oracle/Guardian public keys and       ||\n"
+printf "|| related data.                                                        ||\n"
+printf "||                                                                      ||\n"
+printf "|| It's safe to share your PUBKEYs for your Oracle and Guardian as the  ||\n"
+printf "|| the name implies they are in fact public.                            ||\n"
+printf "||                                                                      ||\n"
+printf "|| PUBKEYs example:                                                     ||\n"
+printf "||   PULL_ORACLE=AbCdEfGhIjKlMnOpQrStUvXwYz1234567890AbCdEfGh           ||\n"
+printf "||   GUARDIAN_ORACLE=AbCdEfGhIjKlMnOpQrStUvXwYz1234567890AbCdEfGh       ||\n"
+printf "||                                                                      ||\n"
+printf "|| The QUEUE variables are fixed among all Oracles & Guardians as they  ||\n"
+printf "|| represent the Solana queues we use to coordinate operations on chain ||\n"
+printf "|| and only change between devnet vs mainnet.                           ||\n"
+printf "||                                                                      ||\n"
+printf "|| QUEUE devnet vars :                                                  ||\n"
+printf "||   PULL_QUEUE=EYiAmGSdsQTuCw413V5BzaruWuCCSDgTPtBGvLkXHbe7            ||\n"
+printf "||   GUARDIAN_QUEUE=BeZ4tU4HNe2fGQGUzJmNS2UU2TcZdMUUgnCH6RPg4Dpi        ||\n"
+printf "|| QUEUE mainnet vars :                                                 ||\n"
+printf "||   PULL_QUEUE=A43DyUGA7s8eXPxqEjJY6EBu1KKbNgfxF8h17VAHn13w            ||\n"
+printf "||   GUARDIAN_QUEUE=B7WgdyAgzK7yGoxfsBaNnY6d41bTybTzEh4ZuQosnvLK        ||\n"
+printf "||                                                                      ||\n"
+printf "|| SECURITY WARNING:                                                    ||\n"
+printf "||  Keep your 24-word seed phrase and private key (array-like number)   ||\n"
+printf "||                         ABSOLUTELY PRIVATE!                          ||\n"
+printf "||                                                                      ||\n"
+printf "|| Next steps:                                                          ||\n"
+printf "||   1. Edit cfg/00-devnet-vars.cfg or cfg/00-mainnet-vars.cfg file     ||\n"
+printf "||      and insert the output in the proper variables                   ||\n"
+printf "||                                                                      ||\n"
+printf "||   2. Submit a request for approval of your Oracle/Guardian data at:  ||\n"
+printf "||                                                                      ||\n"
+printf "||      https://forms.gle/2xWwFQ8XPBGu9DRL6                             ||\n"
+printf "||                                                                      ||\n"
+printf "||      In the form you should just submit the values of                ||\n"
+printf "||        PULL_ORACLE     + PULL_QUEUE                                  ||\n"
+printf "||                                                                      ||\n"
+printf "||      and only if you are runnning a guardian also send               ||\n"
+printf "||        GUARDIAN_ORACLE + GUARDIAN_QUEUE                              ||\n"
+printf "||                                                                      ||\n"
+printf "||   3. Please save the output above and then you can exit now          ||\n"
+printf "||      Wait for approval before proceeding with the following steps    ||\n"
+printf "||                                                                      ||\n"
+printf "||          SAVE THIS OUTPUT NOW, THEN TYPE 'exit' TO CONTINUE          ||\n"
+printf "==========================================================================\n"
+printf "\n"
