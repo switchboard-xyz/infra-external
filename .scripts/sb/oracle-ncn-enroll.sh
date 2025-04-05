edc0e0ad7d847375e6907ae2631da2d2bf572642
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-04-05 03:08:10 +0200

    NCN script: fix output

--- .scripts/sb/oracle-ncn-enroll.sh
+++ .scripts/sb/oracle-ncn-enroll.sh
@@ -114,9 +114,9 @@ export NCN_OPERATOR_ADMIN="$(jito-restaking-cli restaking operator get ${NCN_OPE
 export ORACLE_OPERATOR_AUTHORITY="$(sb solana on-demand oracle print --cluster ${cluster} --rpcUrl ${RPC_URL} ${ORACLE_OPERATOR} 2>&1 | awk '/authority/ {print $(NF)}')"
 
 if [[ "${NCN_OPERATOR_ADMIN}" == "${ORACLE_OPERATOR_AUTHORITY}" ]]; then
-  jito-restaking-cli restaking operator initialize-operator-vault-ticket ${NCN_OPERATOR} ${VAULT} --rpc-url ${RPC_URL} --keypair ${NCN_PAYER_FILE}
-  jito-restaking-cli restaking operator warmup-operator-vault-ticket ${NCN_OPERATOR} ${VAULT} --rpc-url ${RPC_URL} --keypair ${NCN_PAYER_FILE}
-  sb solana on-demand oracle setOperator ${ORACLE_OPERATOR} --operator ${NCN_OPERATOR} --cluster ${cluster} -u ${RPC_URL} -k ${PAYER_FILE}
+  jito-restaking-cli restaking operator initialize-operator-vault-ticket ${NCN_OPERATOR} ${VAULT} --rpc-url ${RPC_URL} --keypair ${NCN_PAYER_FILE} 2>&1 | awk '/Transaction confirmed/ {print $(NF)}'
+  jito-restaking-cli restaking operator warmup-operator-vault-ticket ${NCN_OPERATOR} ${VAULT} --rpc-url ${RPC_URL} --keypair ${NCN_PAYER_FILE} 2>&1 | awk '/Transaction confirmed/ {print $(NF)}'
+  sb solana on-demand oracle setOperator ${ORACLE_OPERATOR} --operator ${NCN_OPERATOR} --cluster ${cluster} -u ${RPC_URL} -k ${PAYER_FILE} 2>&1
 else
   printf "\n"
   printf "==========================================================================\n"
@@ -151,7 +151,15 @@ printf "========================================================================
 printf "\n"
 
 printf "\n"
-printf "jito-restaking-cli restaking operator operator-warmup-ncn ${NCN_OPERATOR} ${NCN} --rpc-url ${RPC_URL} --keypair ${PAYER_FILE}"
+if [[ "${NCN_OPERATOR_ADMIN}" == "${ORACLE_OPERATOR_AUTHORITY}" ]]; then
+  printf "jito-restaking-cli restaking operator operator-warmup-ncn ${NCN_OPERATOR} ${NCN} --rpc-url ${RPC_URL} --keypair ${PAYER_FILE}"
+else
+  if [[ "${DEBUG}" == "true" ]]; then
+    printf "jito-restaking-cli restaking operator operator-warmup-ncn ${NCN_OPERATOR} ${NCN} --rpc-url ${RPC_URL} --keypair NCN_ADMIN_PAYER_FILE"
+  else
+    printf "jito-restaking-cli restaking operator operator-warmup-ncn ${NCN_OPERATOR} ${NCN} --rpc-url ${RPC_URL} --keypair NCN_ADMIN_PAYER_FILE"
+  fi
+fi
 printf "\n"
 
 printf "\n"
