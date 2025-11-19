78d5412209b474b3875c695adf3cef108bdad275
edgar sarkisian <edgar@switchboardlabs.xyz>
2025-11-19 11:46:41 -0500

    fix timescaleDB configs

--- .scripts/kubernetes/k8s-oracle-install.sh
+++ .scripts/kubernetes/k8s-oracle-install.sh
@@ -25,6 +25,8 @@ export ORACLE_DOCKER_IMAGE="docker.io/switchboardlabs/oracle"
 export GUARDIAN_DOCKER_IMAGE="docker.io/switchboardlabs/guardian"
 export GATEWAY_DOCKER_IMAGE="docker.io/switchboardlabs/gateway"
 export OTLP_ENDPOINT="http://sb-log-forwarding-alloy.sb-log-forwarding.svc.cluster.local:4317"
+export TIMESCALEDB_CAPACITY="20Gi"
+export TIMESCALEDB_ENABLED="true"
 
 repo_dir="$(readlink -f ../../..)"
 
@@ -80,7 +82,7 @@ helm upgrade -i "sb-oracle-${NETWORK}" \
   --set components.guardian.image="${GUARDIAN_DOCKER_IMAGE}" \
   --set components.gateway.enabled=${GATEWAY_ENABLED} \
   --set components.gateway.image="${GATEWAY_DOCKER_IMAGE}" \
-  --set components.timescaledb.enabled=${TMESCALEDB_ENABLED} \
+  --set components.timescaledb.enabled=${TIMESCALEDB_ENABLED} \
   --set components.timescaledb.capcity=${TIMESCALEDB_CAPACITY} \
   "${helm_on_demand_chart_dir}" >/dev/null
 printf "HELM: Switchboard Oracle installed under namespace ${NAMESPACE}\n"
