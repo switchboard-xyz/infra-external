45975b983013d46a376a1155f2e4cccfea1af5fa
edgar sarkisian <edgar@switchboardlabs.xyz>
2026-01-28 20:23:23 -0500

    add oracle-updater install script

--- .scripts/kubernetes/k8s-apps-watchtower.sh
+++ .scripts/kubernetes/k8s-apps-watchtower.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/env bash
 set -u -e
 
-echo "HELM: adding Watchtower (Keel) repo"
-helm repo add keel https://charts.keel.sh >/dev/null
-helm repo update >/dev/null
-echo "HELM: Watchtower (Keel) repo added"
+echo "HELM: Checking for existing watchtower releases (replaced in favour of oracle-updater)"
 
-WATCHTOWER_NS="watchtower"
-if [[ "$(kubectl get ns | grep -e '^'${WATCHTOWER_NS}'\W')" == "" ]]; then
-  echo "KUBECTL: creating Namespace ${WATCHTOWER_NS}"
-  kubectl create namespace "${WATCHTOWER_NS}" >/dev/null
-  echo "KUBECTL: Namespace ${WATCHTOWER_NS} created"
+if read -r WATCHTOWER_RELEASE WATCHTOWER_NS _ < <(helm list -A -f "^watchtower$" --no-headers 2>/dev/null); then
+  echo "HELM: Deleting installed Watchtower release in ${WATCHTOWER_NS}"
+  helm uninstall -n "${WATCHTOWER_NS}" "${WATCHTOWER_RELEASE}" >/dev/null
+  echo "HELM: Deleted existing Watchtower release"
+  kubectl delete ns $WATCHTOWER_NS >/dev/null
+  echo "KUBECTL: Deleted watchtower namespace"
 fi
 
-echo "HELM: Installing Watchtower (keel)"
-helm upgrade -i "watchtower" \
-  -n "${WATCHTOWER_NS}" \
-  --set debug="false" \
-  --set helmProvider.enabled="false" \
-  --set polling.defaultSchedule="@every $((74 + RANDOM % 46))s" \
-  keel/keel >/dev/null
-echo "HELM: Watchtower (keel) installed"
+ORACLE_UPDATER_NS="oracle-updater"
+if [[ "$(kubectl get ns | grep -e '^'${ORACLE_UPDATER_NS}'\W')" == "" ]]; then
+  echo "KUBECTL: creating Namespace ${ORACLE_UPDATER_NS}"
+  kubectl create namespace "${ORACLE_UPDATER_NS}" >/dev/null
+  echo "KUBECTL: Namespace ${ORACLE_UPDATER_NS} created"
+fi
+
+helm_dir="${repo_dir}/.scripts/helm/"
+helm_charts_dir="${helm_dir}/charts/"
+helm_oracle_updater_chart_dir="${helm_charts_dir}/oracle-updater/"
+
+echo "HELM: Installing oracle-updater"
+helm upgrade -i "oracle-updater" \
+  -n "${ORACLE_UPDATER_NS}" \
+  "${helm_oracle_updater_chart_dir}" >/dev/null
+
+echo "HELM: oracle-updater installed"
