5ade99344b0de1e3b0f3f51db98890f37726ecf6
edgar sarkisian <edgar@switchboardlabs.xyz>
2026-02-09 17:38:41 -0500

    fix oracle-updater installation

--- .scripts/kubernetes/k8s-apps-updaters.sh
+++ .scripts/kubernetes/k8s-apps-updaters.sh
@@ -43,15 +43,14 @@ helm_oracle_updater_chart_dir="${helm_charts_dir}/oracle-updater/"
 
 cfg_dir="${repo_dir}/cfg"
 cfg_common_file="${cfg_dir}/00-common-vars.cfg"
-cfg_cluster_file="${cfg_dir}/00-${cluster}-vars.cfg"
 
 # import vars
 source "${cfg_common_file}"
-source "${cfg_cluster_file}"
 
 echo "HELM: oracle_updater.image_tag=${DOCKER_IMAGE_TAG}"
 
 helm upgrade -i "oracle-updater" \
   -n "${ORACLE_UPDATER_NS}" \
-  --set oracle_updater.image_tag="${DOCKER_IMAGE_TAG}" \
+  --set oracle_updater.image="${ORACLE_UPDATER_IMAGE=}" \
+  --set oracle_updater.image_tag="${ORACLE_UPDATER_IMAGE_TAG}" \
   "${helm_oracle_updater_chart_dir}" >/dev/null
