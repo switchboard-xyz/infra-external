4b51c207df1bf1c9be5157898d91e8ae7c6ac447
edgar sarkisian <edgar@switchboardlabs.xyz>
2026-02-23 11:04:50 -0500

    switch to f5 nginx

--- .scripts/kubernetes/k8s-apps-ingress-nginx.sh
+++ .scripts/kubernetes/k8s-apps-ingress-nginx.sh
@@ -27,62 +27,58 @@ if [[ "$(type -p helm)" == "" ]]; then
   exit 1
 fi
 
-echo "HELM: adding ingress-nginx repo"
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx >/dev/null
+echo "HELM: adding nginx-stable repo"
+helm repo add nginx-stable https://helm.nginx.com/stable >/dev/null
 helm repo update >/dev/null
-echo "HELM: ingress-nginx repo added"
+echo "HELM: nginx-stable repo added"
 
-echo "HELM: installing ingress-nginx in your cluster"
+echo "HELM: installing nginx-ingress in your cluster"
 if [[ "${platform}" == "azure" ]]; then
   if [[ "${IPv6}" == "0000::0000" || "${IPv6}" == "" ]]; then
-    helm upgrade -i ingress-nginx ingress-nginx/ingress-nginx \
-      --namespace ingress-nginx \
+    helm upgrade -i nginx-ingress nginx-stable/nginx-ingress \
+      --namespace nginx-ingress \
       --create-namespace \
       --timeout 600s \
-      --set controller.kind=DaemonSet \
+      --set controller.kind=daemonset \
       --set controller.service.type=LoadBalancer \
       --set controller.service.externalTrafficPolicy=Local \
       --set controller.nodeSelector."kubernetes\.io/os"=linux \
-      --set defaultBackend.nodeSelector."kubernetes\.io/os"=linux \
       --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-ipv4"="${IPv4}" \
       >/dev/null
   else
-    helm upgrade -i ingress-nginx ingress-nginx/ingress-nginx \
-      --namespace ingress-nginx \
+    helm upgrade -i nginx-ingress nginx-stable/nginx-ingress \
+      --namespace nginx-ingress \
       --create-namespace \
       --timeout 600s \
-      --set controller.kind=DaemonSet \
+      --set controller.kind=daemonset \
       --set controller.service.type=LoadBalancer \
       --set controller.service.externalTrafficPolicy=Local \
       --set controller.nodeSelector."kubernetes\.io/os"=linux \
-      --set defaultBackend.nodeSelector."kubernetes\.io/os"=linux \
       --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-ipv4"="${IPv4}" \
       >/dev/null
   fi
 else
   if [[ "${IPv6}" == "0000::0000" || "${IPv6}" == "" ]]; then
-    helm upgrade -i ingress-nginx ingress-nginx/ingress-nginx \
-      --namespace ingress-nginx \
+    helm upgrade -i nginx-ingress nginx-stable/nginx-ingress \
+      --namespace nginx-ingress \
       --create-namespace \
       --timeout 600s \
-      --set controller.kind=DaemonSet \
+      --set controller.kind=daemonset \
       --set controller.hostNetwork=true \
-      --set controller.hostPort.enabled=true \
       --set controller.service.externalTrafficPolicy=Local \
       --set controller.service.externalIPs[0]="${IPv4}" \
       >/dev/null
   else
-    helm upgrade -i ingress-nginx ingress-nginx/ingress-nginx \
-      --namespace ingress-nginx \
+    helm upgrade -i nginx-ingress nginx-stable/nginx-ingress \
+      --namespace nginx-ingress \
       --create-namespace \
       --timeout 600s \
-      --set controller.kind=DaemonSet \
+      --set controller.kind=daemonset \
       --set controller.hostNetwork=true \
-      --set controller.hostPort.enabled=true \
       --set controller.service.externalTrafficPolicy=Local \
       --set controller.service.externalIPs[0]="${IPv4}" \
       --set controller.service.externalIPs[1]="${IPv6}" \
       >/dev/null
   fi
 fi
-echo "HELM: ingress-nginx installed"
+echo "HELM: nginx-ingress installed"
