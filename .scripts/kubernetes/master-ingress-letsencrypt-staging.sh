910d5053ca681e80255ae2b35caeeaa61ec27d82
edgar sarkisian <edgar@switchboardlabs.xyz>
2026-02-25 14:48:33 -0500

    delete old ingresses with conflicting hosts

--- .scripts/kubernetes/master-ingress-letsencrypt-staging.sh
+++ .scripts/kubernetes/master-ingress-letsencrypt-staging.sh
@@ -34,6 +34,23 @@ cat >"${TMP_FILE}" <<-EOF
 	  - host: ${CLUSTER_DOMAIN}
 EOF
 
+printf "KUBECTL: checking for conflicting Ingresses on host ${CLUSTER_DOMAIN}\n"
+while read -r ns name; do
+  type=$(kubectl get ingress "$name" -n "$ns" \
+    -o jsonpath='{.metadata.annotations.nginx\.org/mergeable-ingress-type}' 2>/dev/null)
+  # skip minions and our own master (already managed by this script)
+  [[ "$type" == "minion" ]] && continue
+  [[ "$name" == "ingress-master" && "$ns" == "default" ]] && continue
+  printf "KUBECTL: deleting conflicting Ingress %s in namespace %s\n" "$name" "$ns"
+  kubectl delete ingress "$name" -n "$ns" >/dev/null
+done < <(
+  kubectl get ingress --all-namespaces \
+    -o custom-columns='NS:.metadata.namespace,NAME:.metadata.name,HOST:.spec.rules[0].host' \
+    --no-headers 2>/dev/null \
+  | awk -v host="${CLUSTER_DOMAIN}" '$3 == host {print $1, $2}'
+)
+printf "KUBECTL: conflict check done\n"
+
 printf "KUBECTL: Deploying master Ingress with letsencrypt-staging\n"
 kubectl apply -f "${TMP_FILE}" >/dev/null
 rm "${TMP_FILE}"
