1a715aac5dbd9278fefd6e73094b0dda9a163e31
edgar sarkisian <edgar@switchboardlabs.xyz>
2026-03-02 17:40:29 -0500

    fix tabs in master-ingress-letsencrypt-prod.sh

--- .scripts/kubernetes/master-ingress-letsencrypt-prod.sh
+++ .scripts/kubernetes/master-ingress-letsencrypt-prod.sh
@@ -5,8 +5,8 @@ set -u -e
 source ../../../cfg/00-common-vars.cfg
 
 if [[ "${IPv4}" == "0.0.0.0" || "${IPv4}" == "" ]]; then
-  echo "INVALID IPv4 - Please fill out correctly all details in \$REPO/cfg/00-common-vars.cfg"
-  exit 1
+	echo "INVALID IPv4 - Please fill out correctly all details in \$REPO/cfg/00-common-vars.cfg"
+	exit 1
 fi
 
 ingressClass="nginx"
@@ -14,24 +14,24 @@ ingressClass="nginx"
 TMP_FILE="./ingress-master.yml"
 
 cat >"${TMP_FILE}" <<-EOF
-  ---
-  apiVersion: networking.k8s.io/v1
-  kind: Ingress
-  metadata:
-    name: ingress-master
-    namespace: default
-    annotations:
-      cert-manager.io/cluster-issuer: letsencrypt-production
-      nginx.org/server-tokens: "false"
-      nginx.org/mergeable-ingress-type: "master"
-  spec:
-    ingressClassName: ${ingressClass}
-    tls:
-    - hosts:
-      - ${CLUSTER_DOMAIN}
-      secretName: ingress-tls
-    rules:
-    - host: ${CLUSTER_DOMAIN}
+	---
+	apiVersion: networking.k8s.io/v1
+	kind: Ingress
+	metadata:
+		name: ingress-master
+		namespace: default
+		annotations:
+			cert-manager.io/cluster-issuer: letsencrypt-production
+			nginx.org/server-tokens: "false"
+			nginx.org/mergeable-ingress-type: "master"
+	spec:
+		ingressClassName: ${ingressClass}
+		tls:
+		- hosts:
+			- ${CLUSTER_DOMAIN}
+			secretName: ingress-tls
+		rules:
+		- host: ${CLUSTER_DOMAIN}
 EOF
 
 printf "KUBECTL: Promoting master Ingress to letsencrypt-prod\n"
