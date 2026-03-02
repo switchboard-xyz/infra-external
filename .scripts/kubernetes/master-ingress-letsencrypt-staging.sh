fe4950b052606a47231c23c5bd11796782c290ac
edgar sarkisian <edgar@switchboardlabs.xyz>
2026-03-02 17:39:26 -0500

    fix tabs in master-ingress-letsencrypt-staging.sh

--- .scripts/kubernetes/master-ingress-letsencrypt-staging.sh
+++ .scripts/kubernetes/master-ingress-letsencrypt-staging.sh
@@ -14,24 +14,24 @@ ingressClass="nginx"
 TMP_FILE="./ingress-master.yml"
 
 cat >"${TMP_FILE}" <<-EOF
-  ---
-  apiVersion: networking.k8s.io/v1
-  kind: Ingress
-  metadata:
-    name: ingress-master
-    namespace: default
-    annotations:
-      cert-manager.io/cluster-issuer: letsencrypt-staging
-      nginx.org/server-tokens: "false"
-      nginx.org/mergeable-ingress-type: "master"
-  spec:
-    ingressClassName: ${ingressClass}
-    tls:
-    - hosts:
-      - ${CLUSTER_DOMAIN}
-      secretName: ingress-tls
-    rules:
-    - host: ${CLUSTER_DOMAIN}
+	---
+	apiVersion: networking.k8s.io/v1
+	kind: Ingress
+	metadata:
+		name: ingress-master
+		namespace: default
+		annotations:
+			cert-manager.io/cluster-issuer: letsencrypt-staging
+			nginx.org/server-tokens: "false"
+			nginx.org/mergeable-ingress-type: "master"
+	spec:
+		ingressClassName: ${ingressClass}
+		tls:
+		- hosts:
+			- ${CLUSTER_DOMAIN}
+			secretName: ingress-tls
+		rules:
+		- host: ${CLUSTER_DOMAIN}
 EOF
 
 printf "KUBECTL: checking for conflicting Ingresses on host ${CLUSTER_DOMAIN}\n"
