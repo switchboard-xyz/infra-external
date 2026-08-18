e1cb4a64b0217b425db8180818c6d67a6aa4f70e
tamlut-modnys <81378423+tamlut-modnys@users.noreply.github.com>
2026-08-18 00:07:46 -0400

    Quote Helm resource quantities

--- .scripts/helm/charts/on-demand/tests/gateway-readiness-render.sh
+++ .scripts/helm/charts/on-demand/tests/gateway-readiness-render.sh
@@ -23,10 +23,10 @@ render_component_hash() {
     awk '{ print $1 }'
 }
 
-gateway_default_hash="c58f0509140ba830fb4ec39e461f84197318353c326aaa7c0864a3c46ebcb476"
-mainnet_default_hash="bf555d3e30cdee6e8425a8ac12c980bf0af49be1efa6b9ea793861e536b680d6"
-oracle_default_hash="d15ffa5a43940a55074cf6ae3818ff050dcfcdc5bcc9a1f86997816f2989e66a"
-guardian_default_hash="f555dd99131820be7432284645812c4a490c3ac816826c08a4c71a734f8f624f"
+gateway_default_hash="09a4499a0cbe0cd863f2631bcec1fb96101a397f6e68566e12b757b364575862"
+mainnet_default_hash="2d0cace98f173d46f6848cfa5026b549aef14aeea353028454904f317f9ef8ef"
+oracle_default_hash="90c7210703cab5c261b71231dc6b2d50877f7ca395c39ccab25d4a238b997cfa"
+guardian_default_hash="38a0df206e18ddbdea8cb4ec867967d7a2f3004506b3cbccd0559ecfdb9a1f6f"
 
 [[ "$(render_component_hash gateway)" == "${gateway_default_hash}" ]]
 [[ "$(render_component_hash oracle)" == "${oracle_default_hash}" ]]
